a830cb92c73b9d66a4666cc4969523a449cd291d
Gurleen Kaur <gurleen9917@gmail.com>
2024-08-11 18:34:59 +0530

    commit

---
+++ car-rental-main/src/Pages/Contact.jsx
@@ -0,0 +1,69 @@
+import { IconMail, IconMailOpened, IconPhone } from "@tabler/icons-react";
+import Footer from "../components/Footer";
+import HeroPages from "../components/HeroPages";
+import { IconLocation } from "@tabler/icons-react";
+
+function Contact() {
+  return (
+    <>
+      <section className="contact-page">
+        <HeroPages name="Contact" />
+        <div className="container">
+          <div className="contact-div">
+            <div className="contact-div__text">
+              <h2>Need additional information?</h2>
+              <a href="/">
+                <IconPhone /> &nbsp; +91 12345-67890
+              </a>
+              <a href="/">
+                <IconMail /> &nbsp; udrive@carmail.com
+              </a>
+              <a href="/">
+                <IconLocation />
+                &nbsp; Delhi
+              </a>
+            </div>
+            <div className="contact-div__form">
+              <form>
+                <label>
+                  Full Name <b>*</b>
+                </label>
+                <input type="text" placeholder='E.g: "Joe Shmoe"'></input>
+
+                <label>
+                  Email <b>*</b>
+                </label>
+                <input type="email" placeholder="youremail@example.com"></input>
+
+                <label>
+                  Tell us about it <b>*</b>
+                </label>
+                <textarea placeholder="Write Here.."></textarea>
+
+                <button type="submit">
+                  <IconMailOpened />
+                  &nbsp; Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="book-banner">
+          <div className="book-banner__overlay"></div>
+          <div className="container">
+            <div className="text-content">
+              <h2>Book a car by getting in touch with us</h2>
+              <span>
+                <IconPhone width={40} height={40} />
+                <h3>+91 12345-67890</h3>
+              </span>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </section>
+    </>
+  );
+}
+
+export default Contact;
---
+++ car-rental-main/src/components/Download.jsx
@@ -0,0 +1,22 @@
+import Img1 from "../images/download/appstore.svg";
+import Img2 from "../images/download/googleapp.svg";
+
+function Download() {
+  return (
+    <>
+      <section className="download-section">
+        <div className="container">
+          <div className="download-text">
+            <h2>Download our app to get most out of it</h2>
+            <div className="download-text__btns">
+              <img alt="download_img" src={Img2} />
+              <img alt="download_img" src={Img1} />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Download;
